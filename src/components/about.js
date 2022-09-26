8ca67b6c0147589f7eae868753fd17dc75045552
Mageta Sukulu <magetasukulu@gmail.com>
2022-09-26 10:16:44 +0300

    Update

---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat ad voluptatibus eaque quia nemo odio nisi ipsa aliquam quidem aliquid qui esse soluta alias sunt, vero eveniet? Quibusdam quidem ad dicta nisi magni ab consectetur quo, aliquam deleniti odit id inventore recusandae ipsa laborum illum mollitia, reiciendis accusantium numquam quos.
+            </p>
+        </div>
+    )
+}
+
+export default About
